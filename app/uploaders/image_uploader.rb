f8b55faebd0fe327e397c2ecfbc47d5bcf5d6005
stamander <iiida.atsushi.fh@gmail.com>
2020-01-14 20:40:04 +0900

    config.consider_all_requests_local = true

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,9 +2,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [800, 800]
+
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -44,5 +44,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
- 
+  process resize_to_fit: [800, 800]
 end
