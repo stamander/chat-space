86620f153c2fda3660dee2365a7c2ae8f5df7116
stamander <iiida.atsushi.fh@gmail.com>
2020-01-15 13:23:41 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,15 +25,8 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
-
+# デプロイ処理が終わった後、Unicornを再起動するための記述
 set :linked_files, %w{ config/secrets.yml }
-
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
